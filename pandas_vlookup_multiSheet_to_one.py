acf77468783880f3160e9ab83d87c68fa332f5ba
董董 <772483200@qq.com>
2021-07-22 18:12:14 +0800

    Update pandas_vlookup_multiSheet_to_one.py

--- pandas_vlookup_multiSheet_to_one.py
+++ pandas_vlookup_multiSheet_to_one.py
@@ -6,7 +6,8 @@ import pandas as pd
 import os
 
 
-def vlookup(excel_left,excel_right,cols_need,left_col,right_col):
+# # merge两列数据类型要一致
+def vlookup(excel_left,excel_right,cols_right_need,left_col,right_col):
 	# 参数为None 代表读取所有sheet
 	df_dict = pd.read_excel(excel_left,sheet_name=None)
 	df_right = pd.read_excel(excel_right)
@@ -14,7 +15,8 @@ def vlookup(excel_left,excel_right,cols_need,left_col,right_col):
 
 	with pd.ExcelWriter(f'{name_res}_vlookup_res.xlsx') as writer:
 		for sheet_name,df_sheet in df_dict.items():
-		    df = df_sheet.merge(df_right[cols_need],left_on=left_col, right_on=right_col)
+			cols_right_need.extend(right_col)
+		    df = df_sheet.merge(df_right[cols_right_need],left_on=left_col, right_on=right_col)
 		    df['communityid'] = df['communityid'].astype('string')
 		    df.to_excel(writer,sheet_name=sheet_name,index=False)
 
@@ -23,8 +25,8 @@ if __name__ == '__main__':
 	dic = {
 	'excel_left':'xiaoqu_hebing_str_del_repeat_add_col.xlsx',
 	'excel_right':'111.xlsx',
-	'cols_need':['小区url','状态码'], # excel_right需要的数据列
-	'left_col':'小区url', # 以什么列为基准vlookup
-	'right_col':'小区url' # 以什么列为基准vlookup
+	'cols_right_need':['小区url','状态码'], # excel_right到excel_left的列
+	'left_col':['小区url'], # 以什么列为基准vlookup
+	'right_col':['小区url'] # 以什么列为基准vlookup
 	}
 	vlookup(**dic)
